3a511c7943f3e46e034fb13d5ca3a77591ceb0fb
Zach <briggszj@gmail.com>
2013-05-26 14:10:18 -0400

    spy service is now an object

--- app/js/services/spy_service.js
+++ app/js/services/spy_service.js
@@ -1,15 +1,17 @@
-function spyService(msg, url, lineNo)  {
-  console.log(bbSpy[0], msg, url, lineNo);
-  var data = {
-    model_state: JSON.stringify(bbSpy),
-    error_message: msg,
-    url: url,
-    line_no: lineNo,
-  };
-  $.ajax({
-    type: "POST",
-    url: '/bb_spy',
-    success: console.log("success post", data),
-    data: data,
-  });
+spyService = {
+  postError: function(msg, url, lineNo)  {
+    console.log(bbSpy[0], msg, url, lineNo);
+    var data = {
+      model_state: JSON.stringify(bbSpy),
+      error_message: msg,
+      url: url,
+      line_no: lineNo,
+    };
+    $.ajax({
+      type: "POST",
+      url: '/bb_spy',
+      success: console.log("success post", data),
+      data: data,
+    });
+  }
 }
